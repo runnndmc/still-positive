019791ee15128c9054a8fa1d5ae6e76f6012c21b
Daynamarie <Dayna.christison@gmail.com>
2020-11-10 16:47:47 -0500

    rendering components on main

---
+++ src/components/LatestEpCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LatestEpCard = () => {
+
+    return(
+        <h3>This is one of the latest <Link to='/episode'>episodes</Link></h3>
+    )
+}
+
+export default LatestEpCard
--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import LatestEpCard from "../components/LatestEpCard";
 import NewestEp from "../components/NewestEp";
 
 const Main = () => {
+    
   return (
     <>
       <h1>Hello Main Page!</h1>
       <NewestEp />
+      <LatestEpCard />
     </>
   );
 };
