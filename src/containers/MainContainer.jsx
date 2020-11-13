081280b8d25860d938c2fee40521e7af3790fdb3
Daynamarie <Dayna.christison@gmail.com>
2020-11-13 16:14:49 -0500

    environment - production

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -11,13 +11,15 @@ import Main from "../screens/Main";
 const MainContainer = () => {
   const [allEpisodes, setAllEpisodes] = useState([]);
 
+  const environment = process.env.NODE_ENV;
+
   useEffect(() => {
     const apiCall = async () => {
       const resp = await axios.get(
         "https://api.airtable.com/v0/appRx1trr4DFayGsm/Table%201?view=Grid%20view",
         {
           headers: {
-            "Authorization": `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
           },
         }
       );
@@ -26,29 +28,32 @@ const MainContainer = () => {
     apiCall();
   }, []);
 
-
-
-
   return (
-    <Switch>
-      <Route exact path="/">
-        <Main />
-      </Route>
-      <Route path="/episode">
-        <EpisodeDetail />
-      </Route>
-      <Route path="/episodes">
-        <AllEpisodes 
-            allEpisodes={allEpisodes}
-            />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/donate">
-        <Donate />
-      </Route>
-    </Switch>
+    <>
+      {environment === "production" ? (
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/episode">
+            <EpisodeDetail />
+          </Route>
+          <Route path="/episodes">
+            <AllEpisodes allEpisodes={allEpisodes} />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/donate">
+            <Donate />
+          </Route>
+        </Switch>
+      ) : (
+        <div>
+          <h2>kitti</h2>
+        </div>
+      )}
+    </>
   );
 };
 
