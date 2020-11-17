f0ab11aaabbe00eb269aee44380412c0fd5291de
Daynamarie <Dayna.christison@gmail.com>
2020-11-16 20:25:10 -0500

    vetrical nav brah - have to do media queries on transform origin

--- src/shared/Nav.jsx
+++ src/shared/Nav.jsx
@@ -6,13 +6,13 @@ import "./nav.css"
 const Nav = () => {
   return (
     <div className='nav-bar'>
-      <Link to="/about">
+      <Link className='nav-link' to="/about">
         <h5>About</h5>
       </Link>
-      <Link to="/episodes">
+      <Link className='nav-link' to="/episodes">
         <h5>Episodes</h5>
       </Link>
-      <Link to="/donate">
+      <Link className='nav-link' to="/donate">
         <h5>Donate</h5>
       </Link>
     </div>
