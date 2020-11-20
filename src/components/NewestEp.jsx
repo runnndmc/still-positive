ab16662446963f297b7cfce51109bc6961af0056
Daynamarie <Dayna.christison@gmail.com>
2020-11-20 11:07:30 -0500

    Audio rendering and playing on main page for the newest episode

--- src/components/NewestEp.jsx
+++ src/components/NewestEp.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-
 const NewestEp = (props) => {
-const {queriedEps} = props
+  const { queriedEps } = props;
 
-console.log()
+  const audioFile = queriedEps.fields.audio_attach[0];
+
+  console.log();
   return (
     <>
-    <h2>{queriedEps.fields.title}</h2>
-    
+      <h2>{queriedEps.fields.title}</h2>
+      <audio controls>
+        <source src={audioFile} type={audioFile}></source>
+        Your browser does not support this audio.
+      </audio>
     </>
-     
   );
 };
 
