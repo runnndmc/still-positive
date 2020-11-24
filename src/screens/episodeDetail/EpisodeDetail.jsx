5f0864583955a50d8fd8c5ec557819341eec6fa8
Daynamarie <Dayna.christison@gmail.com>
2020-11-23 22:52:23 -0500

    detail rough css

--- src/screens/episodeDetail/EpisodeDetail.jsx
+++ src/screens/episodeDetail/EpisodeDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 import Layout from "../../shared/Layout";
-import './episodeDetail.css'
+import "./episodeDetail.css";
 
 const EpisodeDetail = () => {
   const [episode, setEpisode] = useState(null);
@@ -39,40 +39,45 @@ const EpisodeDetail = () => {
   }
 
   if (!isLoaded) {
-    return <h4>Loading...</h4>;
+    return <h4>One Minute Babe...</h4>;
   }
 
   return (
     <Layout>
-      <article className='detail-wrapper'>
-      <p className='detail-episode-num'>{episode.id}</p>
-      <div className='top-wrap'>
-        <h2 className='detail-title'>{episode.title}</h2>
-        <img
-          className="detail-thumbnail"
-          src={episode.thumbnail[0].thumbnails.large.url}
-          alt="Still Positive logo. A cartoon reed-sternberg cell smiling with an outline of a person standing next to it and giving it the finger because - fuck illness"
-          loading="lazy"
-        ></img>
-      </div>
-      <p className='detail-duration'>{episode.duration}</p>
-      {formatDate(episode.post_date)}
-      <iframe
-         className='detail-video'
-        title={episode.title}
-        src={episode.video_link}
-        height="130"
-        width="230"
-        frameBorder="0"
-        allowFullScreen="allowfullscreen"
-        controls="1"
-        loading="eager"
-      ></iframe>
-      <audio className='detail-audio' controls>
-        <source src={episode.audio_attach[0].url} type={episode.audio_attach[0].type}></source>
-        Your browser does not support this audio.
-      </audio>
-      <p className='detail-description'>{episode.description}</p>
+      <article className="detail-wrapper">
+        <section className="wrap">
+          <p className="detail-episode-num">Episode {episode.id}</p>
+          <img
+            className="detail-thumbnail"
+            src={episode.thumbnail[0].thumbnails.large.url}
+            alt="Still Positive logo. A cartoon reed-sternberg cell smiling with an outline of a person standing next to it and giving it the finger because - fuck illness"
+            loading="lazy"
+          ></img>
+        </section>
+        <h2 className="detail-title">{episode.title}</h2>
+        <section className="middle-wrap">
+          <p className="detail-duration">{episode.duration}</p>
+          <span>{formatDate(episode.post_date)}</span>
+        </section>
+        <iframe
+          className="detail-video"
+          title={episode.title}
+          src={episode.video_link}
+          height="125"
+          width="222"
+          frameBorder="0"
+          allowFullScreen="allowfullscreen"
+          controls="1"
+          loading="eager"
+        ></iframe>
+        <audio className="detail-audio" controls>
+          <source
+            src={episode.audio_attach[0].url}
+            type={episode.audio_attach[0].type}
+          ></source>
+          Your browser does not support this audio.
+        </audio>
+        <p className="detail-description">{episode.description}</p>
       </article>
     </Layout>
   );
