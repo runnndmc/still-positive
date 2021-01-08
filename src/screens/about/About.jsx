b9de99e0a237209eacc4eb52474edd74407b9d92
Daynamarie <Dayna.christison@gmail.com>
2021-01-07 22:12:06 -0500

    about with new layout to fit

--- src/screens/about/About.jsx
+++ src/screens/about/About.jsx
@@ -6,7 +6,7 @@ import "./about.css";
 const About = () => {
   return (
     <Layout>
-       <h2 className="about-title">About <em>STP</em></h2>
+       <h2 className="about-title">About STP</h2>
       <section className="about-wrapper">
        
         <p className="about-p1">
