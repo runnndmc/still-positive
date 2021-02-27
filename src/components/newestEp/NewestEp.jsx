b450d16031290e91d556c1888d6db8c3d0b10221
Daynamarie <Dayna.christison@gmail.com>
2021-02-27 16:21:05 -0500

    date change and adjust

--- src/components/newestEp/NewestEp.jsx
+++ src/components/newestEp/NewestEp.jsx
@@ -9,7 +9,7 @@ const NewestEp = (props) => {
   function formatDate(date) {
     var d = new Date(date),
       month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(),
+      day = "" + (d.getDate() + 1),
       year = d.getFullYear();
 
     if (month.length < 2) month = "0" + month;
@@ -35,7 +35,7 @@ const NewestEp = (props) => {
         width="96%"
         height="130"
         scrolling="no"
-        frameborder="no"
+        frameBorder="no"
         allow="autoplay"
         src={queriedEps.fields.audio_url}
       ></iframe>
