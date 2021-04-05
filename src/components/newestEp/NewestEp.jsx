8a57c1a0e30e3547d0257d9e1fe33f2f32da1425
Daynamarie <Dayna.christison@gmail.com>
2021-04-05 16:14:54 -0400

    font adjust and styling on newest episode and episode detail

--- src/components/newestEp/NewestEp.jsx
+++ src/components/newestEp/NewestEp.jsx
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
+
 import "./newestEp.css";
+import {formatDate} from "../../services/date"
 
-const NewestEp = (props) => {
-  const { queriedEps } = props;
+const NewestEp = ({queriedEps}) => {
+  const { fields } = queriedEps;
   const [activated, setActivated] = useState(false);
-
-  function formatDate(date) {
-    var d = new Date(date),
-      month = "" + (d.getMonth() + 1),
-      day = "" + (d.getDate() + 1),
-      year = d.getFullYear();
-
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    return [month, day, year].join("/");
-  }
 
   const epDetailPg = () => {
     return <Redirect to={`/episodes/${queriedEps.id}`} />;
@@ -26,17 +16,17 @@ const NewestEp = (props) => {
     <>
        <h2 className="latest-episode">Newest Episode</h2>
       <section className="top-wrap">
-        <p className="new-episode-num">Ep #{queriedEps.fields.id}</p>
-        <p className="new-date">{formatDate(queriedEps.fields.post_date)}</p>
+        <p className="new-episode-num">Ep #{fields.id}</p>
+        <p className="new-date">{formatDate(fields.post_date)}</p>
       </section>
       <iframe
         className="new-soundcloud"
-        title={queriedEps.fields.title}
+        title={fields.title}
         width="96%"
         height="130"
         scrolling="no"
         frameBorder="no"
-        src={queriedEps.fields.audio_url}
+        src={fields.audio_url}
       ></iframe>
       <button
         onClick={epDetailPg}
